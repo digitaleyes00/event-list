dba960708b21d233ccb22db8f5f6e436ec8c7f82
Brock Dusome <brockdusome@gmail.com>
2018-03-13 12:44:59 -0400

    Pure Component Refactoring
    - Converted components into functional/pure variants
    - Cleaned up icon rendering to match new `renderIcon()`
    - Showcased `{...{a, b}} syntax for injecting named props with the same variables:
    https://reactjs.org/docs/jsx-in-depth.html#spread-attributes

--- client/event/eventDetails.jsx
+++ client/event/eventDetails.jsx
@@ -1,26 +1,20 @@
-const React       = require('react');
-const createClass = require('create-react-class');
-const _           = require('lodash');
-const moment      = require('moment');
 
-const EventDetails = createClass({
-	getDefaultProps() {
-		return { selectedEvent: {} };
-	},
+const React  = require('react');
+const moment = require('moment');
+const _      = require('lodash');
 
-	render() {
-		if(_.isEmpty(this.props.selectedEvent)) {
-			return <p>Click an event to view details.</p>;
-		}
-
-		return <div className='event-details'>
-			<h1>{this.props.selectedEvent.title}</h1>
-			<img src={this.props.selectedEvent.icon} alt={this.props.selectedEvent.title} title={this.props.selectedEvent.title} />
-			<h2>{ moment.unix(this.props.selectedEvent.timestamp).format('MMMM Do YYYY')}</h2>
-			<h3>{ moment.unix(this.props.selectedEvent.timestamp).format('h:mm a')}</h3>
-			{ this.props.renderIcon(this.props.selectedEvent.type) }
-		</div>;
+module.exports = EventDetails = ({
+	selectedEvent = {},
+	renderIcon = () => {},
+}) => {
+	if(_.isEmpty(selectedEvent)) {
+		return <p>Click an event to view details.</p>;
 	}
-});
-
-module.exports = EventDetails;
+	return <div className='event-details'>
+		<h1>{selectedEvent.title}</h1>
+		<img src={selectedEvent.icon} alt={selectedEvent.title} title={selectedEvent.title} />
+		<h2>{ moment.unix(selectedEvent.timestamp).format('MMMM Do YYYY')}</h2>
+		<h3>{ moment.unix(selectedEvent.timestamp).format('h:mm a')}</h3>
+		<i className={`fa fa-2x fa-${renderIcon(selectedEvent.type)}`}/>
+	</div>;
+};
--- client/event/eventList.jsx
+++ client/event/eventList.jsx
@@ -1,31 +1,20 @@
+
 const React         = require('react');
-const createClass	  = require('create-react-class');
 const EventListItem = require('./eventListItem.jsx');
 
-const EventList = createClass({
-	getDefaultProps() {
-		return {
-			events : []
-		};
-	},
+module.exports = EventList = ({
+	events = [],
+	...props,
+}) => {
+	const mappedEvents = events.map((event) => {
+		return <EventListItem
+			key={event.id}
+			event={event}
+			{...props}
+		/>;
+	});
+	return <ul className='event-list'>
+		{ mappedEvents }
+	</ul>;
 
-	render() {
-		const events = this.props.events.map((event) => {
-			const isActive = this.props.selectedEventId === event.id;
-			return <EventListItem
-				key={event.id}
-				event={event}
-				getEvents={this.props.getEvents}
-				renderIcon={this.props.renderIcon}
-				isActive={isActive}
-				setSelectedEvent={this.props.setSelectedEvent}
-				selectedEventId={this.props.selectedEventId} />;
-		});
-
-		return <ul className='event-list'>
-			{ events }
-		</ul>;
-	}
-});
-
-module.exports = EventList;
+};
